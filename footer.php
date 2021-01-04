0ab6f8657c4484048b7807df26804a60e4eb2359
Butrint Selishta <59087042+Butrinti20@users.noreply.github.com>
2021-01-04 15:09:29 +0100

    eeej hajdar

---
+++ footer.php
@@ -0,0 +1,100 @@
+
+<footer class="revealed">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <h3 data-target="#collapse_1">Linqe</h3>
+                <div class="collapse dont-collapse-sm links" id="collapse_1">
+                    <ul>
+                        <li><a href="about.html">Ballina</a></li>
+                        <li><a href="help.html">Të shitura</a></li>
+                        <li><a href="help.html">Rreth Nesh</a></li>
+                        <li><a href="account.html">Si funksionon?</a></li>
+                        <li><a href="blog.html">Kontakti</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h3 data-target="#collapse_2">Kategorite</h3>
+                <div class="collapse dont-collapse-sm links" id="collapse_2">
+                    <ul>
+                        <li><a href="listing-grid-1-full.html">Vetura</a></li>
+                        <li><a href="listing-grid-2-full.html">Motoçikleta (mbi 50cc)</a></li>
+                        <li><a href="listing-grid-1-full.html">Moped (nën 50cc)</a></li>
+                        <li><a href="listing-grid-3.html">Kompjuterë</a></li>
+                        <li><a href="listing-grid-1-full.html">Telefonë</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h3 data-target="#collapse_3">Kontaktet</h3>
+                <div class="collapse dont-collapse-sm contacts" id="collapse_3">
+                    <ul>
+                        <li><i class="ti-home"></i>97845 Baker st. 567<br>Los Angeles - US</li>
+                        <li><i class="ti-headphone-alt"></i>+94 423-23-221</li>
+                        <li><i class="ti-email"></i><a href="#0">info@dealaim.com</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h3 data-target="#collapse_4">ssssssss</h3>
+                <div class="collapse dont-collapse-sm" id="collapse_4">
+                    <div id="newsletter">
+                        <div class="form-group">
+                            <input type="email" name="email_newsletter" id="email_newsletter" class="form-control" placeholder="Your email">
+                            <button type="submit" id="submit-newsletter"><i class="ti-angle-double-right"></i></button>
+                        </div>
+                    </div>
+                    <div class="follow_us">
+                        <h5>Na ndiqni</h5>
+                        <ul>
+                            <li>
+                                <a href="#0"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="img/twitter_icon.svg" alt="" class="lazy"></a>
+                            </li>
+                            <li>
+                                <a href="#0"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="img/facebook_icon.svg" alt="" class="lazy"></a>
+                            </li>
+                            <li>
+                                <a href="#0"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="img/instagram_icon.svg" alt="" class="lazy"></a>
+                            </li>
+                            <li>
+                                <a href="#0"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="img/youtube_icon.svg" alt="" class="lazy"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /row-->
+        <hr>
+        <div class="row add_bottom_25">
+            <div class="col-lg-6">
+
+            </div>
+            <div class="col-lg-6">
+                <ul class="additional_links">
+                    <li><a href="#0">Kushtet e përdorimit</a></li>
+                    <li><a href="#0">Privatësia</a></li>
+                    <li><span>© 2020 DealAim</span></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--/footer-->
+</div>
+<!-- page -->
+
+<div id="toTop"></div>
+<!-- Back to top button -->
+
+<!-- COMMON SCRIPTS -->
+<script src="js/common_scripts.min.js"></script>
+<script src="js/main.js"></script>
+
+<!-- SPECIFIC SCRIPTS -->
+<script src="js/carousel-home.min.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Ansonika">
+    <title>Allaia | Bootstrap eCommerce Template - ThemeForest</title>
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+
+    <!-- GOOGLE WEB FONT -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link rel="preload" href="../css.css?family=Roboto:300,400,500,700,900&display=swap" as="fetch" crossorigin="anonymous">
+    <script>
+        ! function(e, n, t) {
+            "use strict";
+            var o = "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap",
+                r = "__3perf_googleFonts_c2536";
+
+            function c(e) {
+                (n.head || n.body).appendChild(e)
+            }
+
+            function a() {
+                var e = n.createElement("link");
+                e.href = o, e.rel = "stylesheet", c(e)
+            }
+
+            function f(e) {
+                if (!n.getElementById(r)) {
+                    var t = n.createElement("style");
+                    t.id = r, c(t)
+                }
+                n.getElementById(r).innerHTML = e
+            }
+            e.FontFace && e.FontFace.prototype.hasOwnProperty("display") ? (t[r] && f(t[r]), fetch(o).then(function(e) {
+                return e.text()
+            }).then(function(e) {
+                return e.replace(/@font-face {/g, "@font-face{font-display:swap;")
+            }).then(function(e) {
+                return t[r] = e
+            }).then(f).catch(a)) : a()
+        }(window, document, localStorage);
+    </script>
+
+
+    <!-- BASE CSS -->
+    <link href="css/bootstrap.custom.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- SPECIFIC CSS -->
+    <link href="css/home_1.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div id="page">
+
+        <header class="version_1">
+            <div class="layer"></div>
+            <!-- Mobile menu overlay mask -->
+            <div class="main_header">
+                <div class="container">
+                    <div class="row small-gutters">
+                        <div class="col-xl-3 col-lg-3 d-lg-flex align-items-center">
+                            <div id="logo">
+                                <a href="index.html"><img src="img/logo.svg" alt="" width="100" height="35"></a>
+                            </div>
+                        </div>
+                        <nav class="col-xl-6 col-lg-7">
+                            <a class="open_close" href="javascript:void(0);">
+                                <div class="hamburger hamburger--spin">
+                                    <div class="hamburger-box">
+                                        <div class="hamburger-inner"></div>
+                                    </div>
+                                </div>
+                            </a>
+                            <!-- Mobile menu button -->
+                            <div class="main-menu">
+                                <div id="header_menu">
+                                    <a href="index.html"><img src="img/logo_black.svg" alt="" width="100" height="35"></a>
+                                    <a href="#" class="open_close" id="close_in"><i class="ti-close"></i></a>
+                                </div>
+                                <ul>
+                                    <li>
+                                        <a href="../item/allaia-ecommerce-html-template/25781982.html" target="_parent">Ballina</a>
+                                    </li>
+                                    <li>
+                                        <a href="../item/allaia-ecommerce-html-template/25781982.html" target="_parent">Të shitura</a>
+                                    </li>
+                                    <li>
+                                        <a href="../item/allaia-ecommerce-html-template/25781982.html" target="_parent">Rreth Nesh</a>
+                                    </li>
+                                    <li>
+                                        <a href="../item/allaia-ecommerce-html-template/25781982.html" target="_parent">Si funksionon?</a>
+                                    </li>
+                                    <li>
+                                        <a href="../item/allaia-ecommerce-html-template/25781982.html" target="_parent">Kontakti</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                            <!--/main-menu -->
+                        </nav>
+                        <div class="col-xl-3 col-lg-2 d-lg-flex align-items-center justify-content-end text-right">
+                            <a class="phone_top" href="tel://9438843343"><strong><span>Kërkoni ndihmë?</span>+383-44-111-222</strong></a>
+                        </div>
+                    </div>
+                    <!-- /row -->
+                </div>
+            </div>
+            <!-- /main_header -->
+
+            <div class="main_nav Sticky">
+                <div class="container">
+                    <div class="row small-gutters">
+                        <div class="col-xl-3 col-lg-3 col-md-3">
+                            <nav class="categories">
+                                <ul class="clearfix">
+                                    <li><span>
+										<a href="#">
+											<span class="hamburger hamburger--spin">
+												<span class="hamburger-box">
+													<span class="hamburger-inner"></span>
+                                        </span>
+                                        </span>
+                                        Categories
+                                        </a>
+                                        </span>
+                                        <div id="menu">
+                                            <ul>
+                                                <li><span><a href="#0">Makina</a></span>
+                                                    <ul>
+                                                        <li><a href="listing-grid-1-full.html">Vetura</a></li>
+                                                        <li><a href="listing-grid-2-full.html">Motoçikleta (mbi 50cc)</a></li>
+                                                        <li><a href="listing-grid-3.html">Moped (nën 50cc)</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><span><a href="#">Elektronik</a></span>
+                                                    <ul>
+                                                        <li><a href="listing-grid-6-sidebar-left.html">Kompjuter</a></li>
+                                                        <li><a href="listing-grid-7-sidebar-right.html">Telefon</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><span><a href="#">Afarizëm</a></span>
+                                                    <ul>
+                                                        <li><a href="listing-grid-2-full.html">Hapësirë afariste</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="col-xl-6 col-lg-7 col-md-6 d-none d-md-block">
+                            <div class="custom-search-input">
+                                <input type="text" placeholder="Kërko produkte">
+                                <button type="submit"><i class="header-icon_search_custom"></i></button>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-2 col-md-3">
+                            <ul class="top_tools">
+                                <li>
+                                    <!-- /dropdown-cart-->
+                                    <li>
+                                        <div class="dropdown dropdown-access">
+                                            <a href="account.html" class="access_link"><span>Account</span></a>
+                                            <div class="dropdown-menu">
+                                                <a href="account.html" class="btn_1">Kyçu ose Regjistrohu</a>
+                                                <ul>
+                                                    <li>
+                                                        <a href="account.html"><i class="ti-user"></i>Profili im</a>
+                                                    </li>
+                                                    <!-- <li>
+												<a href="help.html"><i class="ti-help-alt"></i>Help and Faq</a>
+											</li> -->
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <!-- /dropdown-access-->
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0);" class="btn_search_mob_HIDDEN"><span>Search</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#menu" class="btn_cat_mob">
+                                            <div class="hamburger hamburger--spin" id="hamburger">
+                                                <div class="hamburger-box">
+                                                    <div class="hamburger-inner"></div>
+                                                </div>
+                                            </div>
+                                            Categories
+                                        </a>
+                                    </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /row -->
+                </div>
+                <div class="search_mob_wp">
+                    <input type="text" class="form-control" placeholder="Kërko produkte">
+                    <input type="submit" class="btn_1 full-width" value="Search">
+                </div>
+                <!-- /search_mobile -->
+            </div>
+            <!-- /main_nav -->
+        </header>
+        <!-- /header -->
