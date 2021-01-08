9b393c86fa171f263757b92e3c833324660b3276
Butrint Selishta <59087042+Butrinti20@users.noreply.github.com>
2021-01-09 00:10:33 +0100

    kycu -> texti ne papercut

--- kyçu.php
+++ kyçu.php
@@ -1,5 +1,8 @@
 ﻿<?php
-	require_once("db.php");
+  header('Content-type: text/html; charset=utf-8');
+  
+  require_once("db.php");
+  
 	if(isset($_POST['signup'])){
 		$user = $_POST['username']; $password_1 = $_POST['password_1']; $password_2 = $_POST['password_2']; $fname = $_POST['first_name']; $lname = $_POST['last_name']; $email = $_POST['email']; $phone = $_POST['phone']; $bday = $_POST['bday']; $city = $_POST['city']; $post = $_POST['postcode'];
 		if(empty($user) || empty($password_1) || empty($password_2)){
@@ -9,265 +12,68 @@
 
 			$to = 'butrintse@gmail.com';
 			$subject = "My subject";
-			$txt = "<head>
-
-  <meta charset='utf-8'>
-  <meta http-equiv='x-ua-compatible' content='ie=edge'>
-  <title>Email Confirmation</title>
-  <meta name='viewport' content='width=device-width, initial-scale=1'>
-  <style type='text/css'>
-  /**
-   * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
-   */
-  @media screen {
-    @font-face {
-      font-family: 'Source Sans Pro';
-      font-style: normal;
-      font-weight: 400;
-      src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');
-    }
-    @font-face {
-      font-family: 'Source Sans Pro';
-      font-style: normal;
-      font-weight: 700;
-      src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');
-    }
-  }
-  /**
-   * Avoid browser level font resizing.
-   * 1. Windows Mobile
-   * 2. iOS / OSX
-   */
-  body,
-  table,
-  td,
-  a {
-    -ms-text-size-adjust: 100%; /* 1 */
-    -webkit-text-size-adjust: 100%; /* 2 */
-  }
-  /**
-   * Remove extra space added to tables and cells in Outlook.
-   */
-  table,
-  td {
-    mso-table-rspace: 0pt;
-    mso-table-lspace: 0pt;
-  }
-  /**
-   * Better fluid images in Internet Explorer.
-   */
-  img {
-    -ms-interpolation-mode: bicubic;
-  }
-  /**
-   * Remove blue links for iOS devices.
-   */
-  a[x-apple-data-detectors] {
-    font-family: inherit !important;
-    font-size: inherit !important;
-    font-weight: inherit !important;
-    line-height: inherit !important;
-    color: inherit !important;
-    text-decoration: none !important;
-  }
-  /**
-   * Fix centering issues in Android 4.4.
-   */
-  div[style*='margin: 16px 0;'] {
-    margin: 0 !important;
-  }
-  body {
-    width: 100% !important;
-    height: 100% !important;
-    padding: 0 !important;
-    margin: 0 !important;
-  }
-  /**
-   * Collapse table borders to avoid space between cells.
-   */
-  table {
-    border-collapse: collapse !important;
-  }
-  a {
-    color: #1a82e2;
-  }
-  img {
-    height: auto;
-    line-height: 100%;
-    text-decoration: none;
-    border: 0;
-    outline: none;
-  }
-  </style>
-
-</head>
-<body style='background-color: #e9ecef;'>
-
-  <!-- start preheader -->
-  <div class='preheader' style='display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;'>
-    A preheader is the short summary text that follows the subject line when an email is viewed in the inbox.
-  </div>
-  <!-- end preheader -->
-
-  <!-- start body -->
-  <table border='0' cellpadding='0' cellspacing='0' width='100%'>
-
-    <!-- start logo -->
-    <tr>
-      <td align='center' bgcolor='#e9ecef'>
-        <!--[if (gte mso 9)|(IE)]>
-        <table align='center' border='0' cellpadding='0' cellspacing='0' width='600'>
-        <tr>
-        <td align='center' valign='top' width='600'>
+			$txt = "<html lang='en'>
+      <head>
+       <meta http-equiv='content-type' content='text/html; charset=utf-8'>
+      
+        <title>Email Confirmation</title>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+      
+        <!--[if lt IE 9]>
+          <script src='https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js'></script>
         <![endif]-->
-        <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'>
-          <tr>
-            <td align='center' valign='top' style='padding: 36px 24px;'>
-              <a href='https://sendgrid.com' target='_blank' style='display: inline-block;'>
-                <img src='css/facebook_icon.png' alt='Logo' border='0' width='48' style='display: block; width: 48px; max-width: 48px; min-width: 48px;'>
-              </a>
-            </td>
-          </tr>
-        </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
-      </td>
-    </tr>
-    <!-- end logo -->
-
-    <!-- start hero -->
-    <tr>
-      <td align='center' bgcolor='#e9ecef'>
-        <!--[if (gte mso 9)|(IE)]>
-        <table align='center' border='0' cellpadding='0' cellspacing='0' width='600'>
-        <tr>
-        <td align='center' valign='top' width='600'>
-        <![endif]-->
-        <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'>
-          <tr>
-            <td align='left' bgcolor='#ffffff' style='padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;'>
-              <h1 style='margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;'>Confirm Your Email Address</h1>
-            </td>
-          </tr>
-        </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
-      </td>
-    </tr>
-    <!-- end hero -->
-
-    <!-- start copy block -->
-    <tr>
-      <td align='center' bgcolor='#e9ecef'>
-        <!--[if (gte mso 9)|(IE)]>
-        <table align='center' border='0' cellpadding='0' cellspacing='0' width='600'>
-        <tr>
-        <td align='center' valign='top' width='600'>
-        <![endif]-->
-        <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'>
-
-          <!-- start copy -->
-          <tr>
-            <td align='left' bgcolor='#ffffff' style='padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;'>
-              <p style='margin: 0;'>Tap the button below to confirm your email address. If you didn't create an account with <a href='https://sendgrid.com'>Paste</a>, you can safely delete this email.</p>
-            </td>
-          </tr>
-          <!-- end copy -->
-
-          <!-- start button -->
-          <tr>
-            <td align='left' bgcolor='#ffffff'>
-              <table border='0' cellpadding='0' cellspacing='0' width='100%'>
-                <tr>
-                  <td align='center' bgcolor='#ffffff' style='padding: 12px;'>
-                    <table border='0' cellpadding='0' cellspacing='0'>
-                      <tr>
-                        <td align='center' bgcolor='#1a82e2' style='border-radius: 6px;'>
-                          <a href='https://sendgrid.com' target='_blank' style='display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;'>Do Something Sweet</a>
-                        </td>
-                      </tr>
-                    </table>
-                  </td>
-                </tr>
-              </table>
-            </td>
-          </tr>
-          <!-- end button -->
-
-          <!-- start copy -->
-          <tr>
-            <td align='left' bgcolor='#ffffff' style='padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;'>
-              <p style='margin: 0;'>If that doesn't work, copy and paste the following link in your browser:</p>
-              <p style='margin: 0;'><a href='https://sendgrid.com' target='_blank'>https://same-link-as-button.url/xxx-xxx-xxxx</a></p>
-            </td>
-          </tr>
-          <!-- end copy -->
-
-          <!-- start copy -->
-          <tr>
-            <td align='left' bgcolor='#ffffff' style='padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf'>
-              <p style='margin: 0;'>Cheers,<br> Paste</p>
-            </td>
-          </tr>
-          <!-- end copy -->
-
-        </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
-      </td>
-    </tr>
-    <!-- end copy block -->
-
-    <!-- start footer -->
-    <tr>
-      <td align='center' bgcolor='#e9ecef' style='padding: 24px;'>
-        <!--[if (gte mso 9)|(IE)]>
-        <table align='center' border='0' cellpadding='0' cellspacing='0' width='600'>
-        <tr>
-        <td align='center' valign='top' width='600'>
-        <![endif]-->
-        <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'>
-
-          <!-- start permission -->
-          <tr>
-            <td align='center' bgcolor='#e9ecef' style='padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;'>
-              <p style='margin: 0;'>You received this email because we received a request for [type_of_action] for your account. If you didn't request [type_of_action] you can safely delete this email.</p>
-            </td>
-          </tr>
-          <!-- end permission -->
-
-          <!-- start unsubscribe -->
-          <tr>
-            <td align='center' bgcolor='#e9ecef' style='padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;'>
-              <p style='margin: 0;'>To stop receiving these emails, you can <a href='https://sendgrid.com' target='_blank'>unsubscribe</a> at any time.</p>
-              <p style='margin: 0;'>Paste 1234 S. Broadway St. City, State 12345</p>
-            </td>
-          </tr>
-          <!-- end unsubscribe -->
-
-        </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
-      </td>
-    </tr>
-    <!-- end footer -->
-
-  </table>
-  <!-- end body -->
-
-</body>";
+        
+        <style>
+          body {
+            background-color: #ccc;
+          }
+          .card {
+            width: 65%;
+            background-color: #f1f1f1;
+            margin:  50px auto;
+            text-align: center;
+            padding: 35px 15px;
+            border-radius: 4px;   
+          }
+          .message {
+            padding-bottom: 30px;
+          }
+          .button {
+            color: #fff;
+            background-color: #6435c9;
+            text-decoration: none;
+            padding: 12px 15px;
+            border-radius: 4px;
+          }
+          .link-text {
+            margin-top: 30px;
+          }
+          .footer {
+            margin-top: 30px;
+            text-align: center;
+          }
+        </style>
+      </head>
+      <body>
+        <div class='card'>
+          <h1 class='h1-font'>Konfirmimi i Emailit</h1>
+          <p class='message'>
+            Përshëndetje @USER, ju falemnderojmë që dëshironi të bashkoheni me <b>DealAim</b>.  
+            Ju lutem klikoni në butonin e më poshtëm për të verifikuar emailin.
+          </p>  
+          <a class='button' href='#'>Verifiko emailin</a>
+          <p class='link-text'>
+            ose kopjojeni linkun e mëposhtem dhe vendoseni ne një nga browseret tuaj:<br />
+            @LINK
+          </p>
+        </div>
+        <p class='footer'>
+          Email i dërguar nga DealAim <br />
+          Copyright © 2021 DealAim. Të gjitha të drejtat e rezervuara
+        </p>
+      </body>
+      </html>";
 			$headers = "From: email_confirm@dealaim.com" . "\r\n" ;
 			$headers .= "MIME-Version: 1.0\r\n";
 			$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
